9cec99cf121017376a7d344bd77d9103c414b9e4
student <a.tsymbaliuk2007@gmail.com>
2023-06-01 20:35:24 +0300

    OOOOO

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 class Student:
     def __init__(self, name, age):
-        self.name=name
-        self.age=age
-        print(name, age)
-Student("John", 22)
+        self.name = name
+        self.age = age
+    def get_info(self):
+        print(f'name: {self.name}, age: {self.age}')
+Student = Student(name = "John", age = 22)
+Student.get_info()
