2bb9adf3eeb09f73d3bc362baab45868202aebda
student <a.tsymbaliuk2007@gmail.com>
2023-06-01 20:13:08 +0300

    aaaaaaaaaaaa

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-dict={}
 lst=[]
-for i in range(3):
-    name=input()
-    mark=int(input())
-    dict[name]=mark
-    lst.append(dict[name])
-    lst.append(mark)
-    x=max(lst)
-    print(dict)
-for key, value in dict:
-    if value==x:
-        print(key)
+for i in range(5):
+    a=input()
+    if a[0]=="Q" or a[0]=="W" or a[0]=="E" or a[0]=="R" or a[0]=="T" or a[0]=="Y" or a[0]=="U" or a[0]=="I" or a[0]=="O" or a[0]=="P" or a[0]=="A" or a[0]=="S" or a[0]=="D" or a[0]=="F" or a[0]=="G" or a[0]=="H" or a[0]=="J" or a[0]=="K" or a[0]=="L" or a[0]=="Z" or a[0]=="X" or a[0]=="C" or a[0]=="V" or a[0]=="B" or a[0]=="N" or a[0]=="M":
+        lst.append(a)
+print(lst)
