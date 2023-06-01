3c3daec727eb118a352531fb0f7c4e4447f687f0
student <a.tsymbaliuk2007@gmail.com>
2023-06-01 20:39:36 +0300

    1111111

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-class Student:
-    def __init__(self, name, age):
-        self.name=name
-        self.age=age
-        print(name, age)
-Student("John", 22)
+class BankAccount:
+    def __init__(self, balance, account_number):
+        self.balance = balance
+        self.account_number = account_number
+    def deposit(self):
+        self.balance = self.balance + 100
+        print(self.balance)
+    def withdraw(self):
+        self.balance = self.balance - 100
+        if self.balance>0:
+            print(self.balance)
+bank=BankAccount(balance = 1000, account_number = 777)
+bank.deposit()
+bank.withdraw()
