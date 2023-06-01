093462c52935ed444f648f77491e7eae12b4f1bc
student <a.tsymbaliuk2007@gmail.com>
2023-06-01 20:17:21 +0300

    bvbvbvbvbv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-lst=[]
-for i in range(5):
-    a=input()
-    if a[0]=="Q" or a[0]=="W" or a[0]=="E" or a[0]=="R" or a[0]=="T" or a[0]=="Y" or a[0]=="U" or a[0]=="I" or a[0]=="O" or a[0]=="P" or a[0]=="A" or a[0]=="S" or a[0]=="D" or a[0]=="F" or a[0]=="G" or a[0]=="H" or a[0]=="J" or a[0]=="K" or a[0]=="L" or a[0]=="Z" or a[0]=="X" or a[0]=="C" or a[0]=="V" or a[0]=="B" or a[0]=="N" or a[0]=="M":
-        lst.append(a)
-print(lst)
+a=input()
+b=a.split(" ")
+for i in b:
+    if "Python" in i:
+        print(i)
